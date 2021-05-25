641e2c05d36a86e681b3a3ec4ebba302284ff071
Jacob Motes <jmotes2583@gmail.com>
2021-05-25 17:23:35 -0500

    added artist post and get routes

--- server/server.js
+++ server/server.js
@@ -31,13 +31,38 @@ app.listen(PORT, () => {
 
 
 app.get('/artist', (req, res) => {
-    console.log(`In /songs GET`);
-    res.send(artistList);
+    //need to retrieve all songs from database
+    const queryArtist= `SELECT * FROM "artist" ORDER BY "birthdate" DESC;`
+
+    //need to get query from database
+    pool.query(queryArtist)
+    .then( (result) => {
+        console.log(result.rows);
+        res.send(result.rows);
+
+    }).catch( (err) => {
+        console.log(err);
+        res.sendStatus(500);
+    })
 });
 
+
+
+
 app.post('/artist', (req, res) => {
-    artistList.push(req.body);
-    res.sendStatus(201);
+    
+    let queryText = `INSERT INTO "artist" ("name", "birthdate") 
+    VALUES ($1, $2)`;
+
+    let artistValues = [req.body.name, req.body.birthdate];
+
+    pool.query(queryText, artistValues) 
+    .then( (result) => {
+        res.sendStatus(201);
+    }).catch( (err) => {
+        console.log(err);
+        res.sendStatus(500);
+    })
 });
 
 app.get('/song', (req, res) => {
